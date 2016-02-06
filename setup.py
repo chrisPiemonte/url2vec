e47751e54e9265b0e80f457b500c2eeb89209247
chrisPiemonte <chris.piemonte.nj@gmail.com>
2016-02-07 00:00:12 +0100

    model folder

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='urlembedding',
+      version='1.0',
+      description='Vector representation for URLs',
+      author='Chris Piemonte',
+      url='https://github.com/chrisPiemonte/url2vec',
+      packages=['model', 'util'],
+     )
